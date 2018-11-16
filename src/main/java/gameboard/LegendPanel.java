065f2228dfa44d5a99b1878168098ce5714e1ca9
Aminata Amadou Kindo <aa1184@ship.edu>
2018-11-16 16:50:19 -0500

    updated gameboard legend

--- src/main/java/gameboard/LegendPanel.java
+++ src/main/java/gameboard/LegendPanel.java
@@ -2,15 +2,17 @@ package gameboard;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class LegendPanel extends JPanel {
   public LegendPanel() {
     setLayout(new GridLayout(9,1));
     // Add a border around the panel.
     setBorder(BorderFactory.createTitledBorder("Legend"));
-    ImageIcon human = new ImageIcon("human.jpg");
+    
+    ImageIcon humanIcon = new ImageIcon("/home/aa1184/Fall2018/swe200/labs/lab567-newfolder/lab567/src/main/java/gameboard/human.jpg");
     JLabel humanMsg = new JLabel("   Represents a human");
-    humanMsg.setIcon(human);
+    humanMsg.setIcon(fixImage(humanIcon));
     
     ImageIcon alien = new ImageIcon("alien.jpg");
     JLabel alienMsg = new JLabel("   Represents an alien");
@@ -19,6 +21,18 @@ public class LegendPanel extends JPanel {
     //add legend
     add(humanMsg);
     add(alienMsg);
+    
+    
   }
+  
+  private ImageIcon fixImage(ImageIcon imageIcon) {
+    Image image = imageIcon.getImage(); // transform it 
+    Image newimg = image.getScaledInstance(25, 25,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+    imageIcon = new ImageIcon(newimg);  // transform it back
+
+    return imageIcon;
+}
+  
+  
 
 }
