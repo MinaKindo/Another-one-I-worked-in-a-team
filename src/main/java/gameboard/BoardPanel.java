964f524b1a8285f33fad12ce9c444146ef3ea013
Doris Hartung <dh3187@cs.ship.edu>
2018-11-16 16:48:31 -0500

    changed size of gameboard

--- src/main/java/gameboard/BoardPanel.java
+++ src/main/java/gameboard/BoardPanel.java
@@ -1,6 +1,7 @@
 package gameboard;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
@@ -10,17 +11,18 @@ import javax.swing.JPanel;
 import environment.Cell;
 
 public class BoardPanel extends JPanel {
-	JPanel squares[][] = new JPanel[10][10];
+	JPanel squares[][] = new JPanel[4][4];
 	
 	public BoardPanel() {
 	  
-	  setLayout(new GridLayout(10,10));
+	  setLayout(new GridLayout(4,4));
 	  for (int r = 0; r < squares.length; r++) {
       for (int c = 0; c < squares[0].length; c++) {
         squares[r][c] = new JPanel();
         squares[r][c].setBackground(Color.white);
         squares[r][c].setBorder(BorderFactory.createLineBorder(Color.black));
         add(squares[r][c]);
+        squares[r][c].setPreferredSize(new Dimension (100,100));
       }
     }
 	  
