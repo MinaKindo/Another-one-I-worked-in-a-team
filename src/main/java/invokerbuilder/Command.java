2dea17aaf14b998b31a1b7e8ced864720d290501
Seth Buletza <sb0476@cs.ship.edu>
2018-11-20 12:21:07 -0500

    added Command class

---
+++ src/main/java/invokerbuilder/Command.java
@@ -0,0 +1,42 @@
+/**
+ * @author sb0476
+ */
+package invokerbuilder;
+
+import lifeform.LifeForm;
+import weapon.Weapon;
+
+public class Command {
+  
+  public void reloadCommand(LifeForm lifeform) {
+    lifeform.reload();
+    //weapon.reload();
+  }
+  
+  public void turnPlayerCommands() {
+    //add turning and movement abilities
+  }
+  
+  public void moveCommand() {
+    //add turning and movement abilities
+  }
+  
+  public void attackCommand(LifeForm lifeform, int distance) {
+    LifeForm.attack(lifeform, distance);
+    //change attack() to static?
+  }
+  
+  public void dropCommand(LifeForm lifeform) {
+    LifeForm.dropWeapon();
+    //change dropWeapon() to static?
+  }
+  
+  public void acuireCommand(LifeForm lifeform, Weapon newWeapon) {
+    if(lifeform.hasWeapon() == true) {
+      lifeform.dropWeapon();
+    }
+    
+    lifeform.pickUpWeapon(newWeapon);
+  }
+
+}
