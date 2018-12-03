d4f6ade27a823814c5562b504dfe66b2067226f5
Doris Hartung <dh3187@cs.ship.edu>
2018-12-03 13:20:27 -0500

    added get and set state

--- src/main/java/state/AIContext.java
+++ src/main/java/state/AIContext.java
@@ -9,12 +9,21 @@ public class AIContext {
   private ActionState outOfAmmoState;
   private ActionState deadState;
   private ActionState noWeaponState;
+  public ActionState state;
   
   public AIContext(Environment e, LifeForm l) {
     hasWeaponState = new HasWeaponState(e, this, l);
     outOfAmmoState = new OutOfAmmoState(e, this, l);
     deadState = new DeadState(e, this, l);
     noWeaponState = new NoWeaponState(e, this, l);
+  }
+  
+  public ActionState getState() {
+    return state;
+  }
+  
+  public void setState(ActionState s) {
+    state = s;
   }
   
   public void executeAction() {
